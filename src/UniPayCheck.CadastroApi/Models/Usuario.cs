4e4ee76fb6346a3c62fd82033c0514a68c85ea33
ARVTech <busstationsys@gmail.com>
2023-06-26 15:11:12 -0300

    Organização e estrutura da solution PayCheck.

---
+++ src/UniPayCheck.CadastroApi/Models/Usuario.cs
@@ -0,0 +1,18 @@
+﻿namespace UniPayCheck.CadastroApi.Models;
+
+using System.ComponentModel.DataAnnotations;
+
+public class Usuario
+{
+    [Key]
+    public int IdUsuario { get; set; }
+
+    public int IdPessoaFisica { get; set; }
+
+    public string Password { get; set; }
+
+    public string Token { get; set; }
+
+    public string UserName { get; set; }
+}
+
