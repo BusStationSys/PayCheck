af42722e10ed56922aa804412954edad05968542
ARVTech <busstationsys@gmail.com>
2023-06-27 02:28:27 -0300

    Inclusão dos controllers do Demonstrativo de Pagamento e Espelho Ponto.

---
+++ src/UniPayCheck.App/Controllers/DemonstrativosPagamentoController.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UniPayCheck.App.Controllers
+{
+    public class DemonstrativosPagamentoController : Controller
+    {
+        // GET: DemonstrativosPagamentoController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: DemonstrativosPagamentoController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: DemonstrativosPagamentoController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: DemonstrativosPagamentoController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: DemonstrativosPagamentoController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: DemonstrativosPagamentoController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: DemonstrativosPagamentoController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: DemonstrativosPagamentoController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
