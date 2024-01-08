20b410b63eada7c91336477f0c995dc154d21f5d
André Rodrigues de Vargas <andrerv_vb@yahoo.com.br>
2024-01-08 00:11:59 -0300

    Implementação da tela dos Usuários.

---
+++ src/PayCheck.Web/Models/UsuarioViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace PayCheck.Web.Models;
+
+using System;
+using System.ComponentModel.DataAnnotations;
+
+public class UsuarioViewModel
+{
+    [Key]
+    public Guid? Guid { get; set; }
+
+    public string Email { get; set; }
+
+    public string Username { get; set; }
+}
