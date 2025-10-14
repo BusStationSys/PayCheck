d17afee1f837fed41d69d0fb47695d396bc13971
André Rodrigues de Vargas <andrerv_vb@yahoo.com.br>
2025-10-14 00:05:47 -0300

    Relatório de Pendências de MDP.

---
+++ src/PayCheck.Web/Models/RelatorioPendenciaDemonstrativoPagamentoViewModel.cs
@@ -0,0 +1,17 @@
+﻿namespace PayCheck.Web.Models
+{
+    using System.ComponentModel.DataAnnotations;
+    using ARVTech.Shared.Enums;
+    
+    public class RelatorioPendenciaDemonstrativoPagamentoViewModel
+    {
+        [Display(Name = "Competência Inicial")]
+        public string CompetenciaInicial { get; set; }
+
+        [Display(Name = "Competência Final")]
+        public string CompetenciaFinal { get; set; }
+
+        [Display(Name = "Situação")]
+        public SituacaoPendenciaDemonstrativoPagamento Situacao { get; set; }
+    }
+}
